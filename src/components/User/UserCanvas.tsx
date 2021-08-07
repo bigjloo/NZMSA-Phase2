07f10ae0c2a13b7e98a4f9bd0f7bd066f5a63b80
Jay <junzhongloo@gmail.com>
2021-08-07 13:54:00 +1200

    camera test

--- src/components/User/UserCanvas.tsx
+++ src/components/User/UserCanvas.tsx
@@ -1,12 +1,32 @@
+import { useState } from "react"
 import EventTimeline from "../Event/EventTimeline"
+import { Camera } from "../MediaCapture/Camera"
 
 const UserCanvas = () => {
+  const [isCameraOpen, setIsCameraOpen] = useState(false)
+  const [cardImage, setCardImage] = useState()
   // TOOD
   // styling
   return (
     <>
       <h1>Canvas</h1>;
       <EventTimeline />
+      {isCameraOpen && (
+        <Camera
+          onCapture={(blob: any) => setCardImage(blob)}
+          onClear={() => setCardImage(undefined)}
+        />
+      )}
+      {cardImage && (
+        <div>
+          <h2>Preview</h2>
+          <img
+            alt="preview"
+            src={cardImage && URL.createObjectURL(cardImage)}
+          />
+        </div>
+      )}
+      <button onClick={() => setIsCameraOpen(true)}>Open Camera</button>
     </>
   )
 }
